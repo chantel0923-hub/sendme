c84446853b655cc7a640ce57f8f403384e60cee3
Donald van Wyk [FIRE OPS SA] <donald@fireopssa.co.za>
2026-06-01 21:46:44 +0200

    Added Supabase config and database tables

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = pidlanyedgieiyxuipwf;
+const SUPABASE_KEY = sb_publishable_QCYr1YvSishTG8IhyGcTTw_v-7bcyzI;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
